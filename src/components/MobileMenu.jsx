5f23acdf56a08b511116a07b157e1745bd9dbea8
vpranavmadhu <pranavmadhu2003@gmail.com>
2025-09-27 20:08:22 +0530

    Solves error in mobile menu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -10,7 +10,7 @@ const MobileMenu = ({menuOpen, setMenuOpen}) => {
        <a href="#home" className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300 ${menuOpen ? "opacity-100 translate-y-0": "opacity-0 translate-y-5"}`} onClick={()=> setMenuOpen(false)}>Home</a>
                     <a href="#about" className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300 ${menuOpen ? "opacity-100 translate-y-0": "opacity-0 translate-y-5"}`} onClick={()=> setMenuOpen(false)}>About</a>
                     <a href="#projects" className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300 ${menuOpen ? "opacity-100 translate-y-0": "opacity-0 translate-y-5"}`} onClick={()=> setMenuOpen(false)}>Projects</a>
-                    <a href="contact" className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300 ${menuOpen ? "opacity-100 translate-y-0": "opacity-0 translate-y-5"}`} onClick={()=> setMenuOpen(false)}>Contact</a>
+                    <a href="#contact" className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300 ${menuOpen ? "opacity-100 translate-y-0": "opacity-0 translate-y-5"}`} onClick={()=> setMenuOpen(false)}>Contact</a>
 
     </div>
   )
